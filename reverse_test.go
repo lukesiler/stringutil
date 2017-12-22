18c757dd3b704b1ce86f38053c17a11437480161
Luke Siler <lukesiler@gmail.com>
2017-12-22 15:46:55 -0800

    add neg test

--- reverse_test.go
+++ reverse_test.go
@@ -3,17 +3,32 @@ package stringutil
 import "testing"
 
 func TestReverse(t *testing.T) {
-	cases := []struct {
+	posCases := []struct {
 		in, want string
 	}{
 		{"Hello, world", "dlrow ,olleH"},
 		{"Hello, 世界", "界世 ,olleH"},
 		{"", ""},
 	}
-	for _, c := range cases {
+	for _, c := range posCases {
 		got := Reverse(c.in)
 		if got != c.want {
 			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+
+	negCases := []struct {
+		in, noWant string
+	}{
+		{"Hello, world", "dlrow, olleH"},
+		{"Hello, 世界", "界世, olleH"},
+		{"", " "},
+		{"something", "nothing"},
+	}
+	for _, c := range negCases {
+		got := Reverse(c.in)
+		if got == c.noWant {
+			t.Errorf("Reverse(%q) == %q, don't want %q", c.in, got, c.noWant)
 		}
 	}
 }
